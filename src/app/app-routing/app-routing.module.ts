2b57e4e255a15a857d45f85794d5e74a6beb3dcf
Miguel Costa <m_costa3@etu-webschoolfactory.fr>
2020-05-21 20:14:29 +0200

    logic v1

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -3,11 +3,19 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from '../auth/auth.component';
 import { AdminComponent } from '../admin/admin.component';
+import { AuthGuard } from '../auth.guard';
+import {LoginComponent} from '../login/login.component';
 
+/* Routes */
 const routes: Routes = [
+  /* Redirect the empty path to the auth path */
   { path: '', pathMatch: 'full', redirectTo: 'login'},
+  /* Route for login component */
+  { path: 'login', component: LoginComponent},
+  /* Route for auth component */
   { path: 'auth', component: AuthComponent },
-  { path: 'admin', component: AdminComponent }
+  /* Route for the admin component, canActivate guard the route for user not logged */
+  { path: 'admin', component: AdminComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
 import { AdminComponent } from './admin/admin.component';
-import {AppRoutingModule} from './app-routing/app-routing.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthComponent,
-    AdminComponent
+    AdminComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
