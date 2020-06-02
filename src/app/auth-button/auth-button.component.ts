2eb1b4531e33d30aa43fb6cd750c074f371d2f1a
Miguel Costa <m_costa3@etu-webschoolfactory.fr>
2020-06-02 14:51:05 +0200

    google sign in added and route

--- src/app/auth-button/auth-button.component.ts
+++ src/app/auth-button/auth-button.component.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-auth-button',
   templateUrl: './auth-button.component.html',
-  styleUrls: ['./auth-button.component.css']
+  styleUrls: [ './auth-button.component.css' ]
 })
 export class AuthButtonComponent implements OnInit {
+  private isSubmitted: boolean;
   private authInstance: any;
   private user: any;
   private error: any;
   private gapiSetup: boolean;
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) {
+  }
+
 
   async ngOnInit() {
     if (await this.checkIfUserAuthenticated()) {
@@ -26,12 +31,11 @@ export class AuthButtonComponent implements OnInit {
     const pload = new Promise((resolve) => {
       gapi.load('auth2', resolve);
     });
-
     // When the first promise resolves, it means we have gapi
     // loaded and that we can call gapi.init
     return pload.then(async () => {
       await gapi.auth2
-        .init({ client_id: '115222645491-up14vifdmmfes7v76h6saug0mfe7hf0j.apps.googleusercontent.com' })
+        .init({client_id: '115222645491-up14vifdmmfes7v76h6saug0mfe7hf0j.apps.googleusercontent.com'})
         .then(auth => {
           this.gapiSetup = true;
           this.authInstance = auth;
@@ -44,12 +48,13 @@ export class AuthButtonComponent implements OnInit {
     if (!this.gapiSetup) {
       await this.initGoogleAuth();
     }
-
     // Resolve or reject signin Promise
     return new Promise(async () => {
       await this.authInstance.signIn().then(
         user => this.user = user,
-        error => this.error = error);
+        error => this.error = error,
+        this.authService.signIn(this.user),
+        this.router.navigateByUrl('/admin'));
     });
   }
 
@@ -58,8 +63,6 @@ export class AuthButtonComponent implements OnInit {
     if (!this.gapiSetup) {
       await this.initGoogleAuth();
     }
-
     return this.authInstance.isSignedIn.get();
   }
-
 }
